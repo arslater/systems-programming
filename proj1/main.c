d9b0912eafcc9189fd91a588d5a3b6d3bf3b2107
anita <slater.anita@gmail.com>
2019-02-21 21:27:58 -0500

    [proj1] main now accepts command line arguments, and outputs to a .out file

--- proj1/main.c
+++ proj1/main.c
@@ -2,22 +2,42 @@
 #include "data.h"
 #include <stdlib.h>
 
+int OPERAND = 16;
+
 int main(int argc, char ** argv)
 {
 	FILE *fp;
-	char * string ="(((AYYYY+B)*C)-((D-E)*(F+G)))";
-	Tree *expTree = makeTree();
+//	char * string = (char *) malloc(sizeof(char)*256);
 
-	string = "((5+4)+(7*9))"; 
-//	string = "((5+4)*3)";
+	if((fp=fopen(argv[1],"r"))== NULL)
+	{
+		fprintf(stderr,"ERROR: cannot open '%s' for reading! Exiting...\n",argv[1]);
+		exit(1);
+	}
+	char * string = (char *) malloc(sizeof(char)*64);
+	int i =0;
+	Tree *expTree = makeTree();
 	Stack *myStack = makeStack();
+	char * outfile = strcat(strtok(argv[1],"."),".out");
+	while( !feof(fp))
+	{
+		string[i] = fgetc(fp);
+		i++;
+	}
+	string[i-2] = '\0';
+
+	fclose(fp);
+
+	if(( fp=fopen(outfile,"w")) == NULL)
+	{
+		fprintf(stderr,"Error opening '%s' for writing. Exiting!\n",outfile);
+		exit(2);
+	}
+
 	myStack = infix2postfix(string);
 	myStack = reverseStack(myStack);
-	//printf("Finsihed with no errors!%s\n",myStack->bottom ->data);
-	printf("%s\n", stack2string(myStack, strlen(string)));
 	
 	expTree = buildTree(myStack);
-	printTree(expTree->root);
-	printf("%d\n",evaluate(expTree->root));
+	fprintf(fp,"%s=%lf\n",string,evaluate(expTree->root));
 	return 0;
 }
