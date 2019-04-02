db424a40fbf0ad5107ddd95b9dc068e92eb72f81
Anita Slater <arslater@albany.edu>
2019-04-02 22:19:02 +0000

    [proj4] Have everything working and USES/TDN documentation

--- proj4/consumer.c
+++ proj4/consumer.c
@@ -12,43 +12,35 @@ void main()
 
     int i=0;
     int j=0;
-    char *digit = (char *) malloc(sizeof(char)*2) ;
-    char * ptr;
     long sum = 0;
     long num = 0;
-    /////////////////////
-    // want to read from a file
-    // should have the same file pointers & descripters
+    char * ptr;
+    char * digit  = (char *) malloc(sizeof(char)*2) ;
     char * stream = (char *) malloc(sizeof(char)*60);
-    //char stream[] = "1 207 3 4 5";
-    fprintf(stdout,"I am a child doing things!!!\n");
-    read(0,stream,60);
-    fprintf(stdout,"%s\n",stream);
+
+    read(0,stream,60); // read from pipe, 0 has been 'casted' to 0 from dup() call
     fprintf(stdout,"Even Numbers:");
 
     do
     {
-        //fprintf(stdout,"##%c|",stream[i]);
         if(stream[i] == ' ' || stream[i] == 0)
         {
             num = strtol(digit,&ptr,10);
             if(num % 2 == 0)    // even number
                 fprintf(stdout,"%ld ",num);
             else
-            {
-                sum += num;
-            }
+                sum += num; // odd number add it to overall sum
             digit[0] = '\0';
             digit[1] = '\0';
             j=0;
         }
         else
         {
-            digit[j] = stream[i];
+            digit[j] = stream[i]; // support for 2-digit numbers
             j++;
         }
         i++;
-    }while(stream[i-1]!=0);
+    }while(stream[i]!=0);
 
     fprintf(stdout,"\nSum of odd values:%ld\n",sum);
 
--- proj4/producer.c
+++ proj4/producer.c
@@ -1,3 +1,9 @@
+/////////////////////////////
+// csi402: proj4
+// producer.c
+//
+// by anita slater
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -9,70 +15,96 @@
 
 int main()
 {
-    // Want to create file first
-    FILE*   file = fopen("numbers","w");;
-    int   fp = open("numbers",O_RDONLY);
+
+    int fp;
+    FILE* file;
     int status = 0;
     int   i;
     pid_t pid;
     int   maxNum      = 20; // random numbers b/w 0 and 20
     int   howManyNums = 20; // stores 20 numbers by default
     char *buff        = (char *) malloc(sizeof(char)*60);
+    char  nums[2];
     int   fd[2];
+    int ran          = 0;
 
+    ////////////////
+    // Try creating file. Make sure that a file of the
+    // same name does not exist
+    remove("numbers");
+    if ((fp =open("numbers", O_RDWR|O_CREAT,00666 )) == -1)
+    {
+        ///////////////////////
+        // Error creating a file with those permissions
+        fprintf(stderr,"Error creating file...\n");
+    }
     srand(time(NULL));
+
+    fprintf(stdout,"File \"numbers\" fp = %d\n",fp);    /* could have used write() or fprintf() for both of*/
+    write(1,"File \"numbers\" contents is: ",28);       /* these. Just wanted practice going back and forth*/
 
     // Generating the file with random numbers
     for(i=0;i<howManyNums;i++)
-        fprintf(file,"%d ", rand()%maxNum);
-    fclose(file); // should it be closed?????????
+    {
+        //////////////////////////
+        // Loading the file with random variables
+        //   Have the same variable being saved in the file
+        //   as well as being outputted to the screen.
+        //   Can also write to the pipe here
+        ran = (rand() % (maxNum-1))+1;
+
+        sprintf(nums, "%d",ran);
+        write(fp,nums,strlen(nums));
+        write(1,nums,strlen(nums));
+        write(fp," ",1);
+        write(1," ",1);
+    }
+    write(1,"\n",1);
+    close(fp);
+
+    // Save the results of the file into a buffer string
     file = fopen("numbers","r");
     fgets(buff,howManyNums*3,file);
+    fclose(file);
 
-    if(pipe(fd)==-1 )
+    if(pipe(fd)==-1 ) // creating the pipe
     {
-        fprintf(stderr, "Pipe failed\n");   // creating the pipe
-        exit(1);
+        fprintf(stderr, "Pipe failed\n");
+        exit(-1);
     }
     if((pid = fork()) == -1)
     {
         ////////////////////
         // Error forking
         fprintf(stderr,"Fork Error. Exiting...\n");
-        exit(1);
+        exit(-1);
     }
     if(pid == 0)
     {
         ///////////////////////
         // We are the child
-       // printf("I am the child!!!\n");
-        //dup2(fd[1],0);
+
+        fprintf(stdout,"Child: My pid = %d. My parent pid = %d\n",getpid(),getppid());
         write(fd[1], buff,strlen(buff)+1);
         dup2(fd[0],0);
-        close(fd[1]);//no more writing
+        close(fd[1]);//no more writing to the pipe
 
         execvp("./consumer",NULL);//run consumer
-        fprintf(stderr,"Exec failed\n");
-        exit(2);
+        fprintf(stderr,"Exec failed\n"); // this should never show
+        exit(-2);
     }
     else
     {
-        if( (pid = wait(&status)) < 0){
+        ///////////////////////////
+        // We are the parent
+        if( (pid = wait(&status)) < 0)
+        {
+            /////////////////////////
+            // Waiting for the child to terminate
             perror("wait");
             _exit(1);
-
         }
-        ////////////////////////
-        // We are the parent
-        if(status == 0)
-            printf("The child process terminated normally\n");
-        else
-            printf("The child process terminated with an error");
-
-        // close the writing pipe
-        //close(fd[1]);
-        //close(fd[0]);
-        //pid = sleep(1);
+        fprintf(stdout,"Parent: My pid = %d. I created child pid = %d\n",getpid(),pid);
     }
     return 0;
 }
